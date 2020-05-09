1ae195d1e67b34f4e32f1c8a288595ad6bc3d16e
ShimantoKabir <maask5534@gmail.com>
2020-05-10 01:38:58 +0600

    vnbv sds

--- ConsultantWebService/src/main/java/com/dannygroupllc/ConsultantWebService/ConsultantWebServiceApplication.java
+++ ConsultantWebService/src/main/java/com/dannygroupllc/ConsultantWebService/ConsultantWebServiceApplication.java
@@ -12,6 +12,7 @@ public class ConsultantWebServiceApplication {
 
 	// public static String machineIp = "3.20.119.226";
 	// public static String machineIp = "192.168.43.132";
+	// test
 	public static void main(String[] args) {
 		SpringApplication.run(ConsultantWebServiceApplication.class, args);
 	}
