9638c94af51245379239f2cd1c733fb53a5b9b8f
ShimantoKabir <maask5534@gmail.com>
2020-05-10 01:49:35 +0600

    ghgjhg

--- ConsultantWebService/src/main/java/com/dannygroupllc/ConsultantWebService/ConsultantWebServiceApplication.java
+++ ConsultantWebService/src/main/java/com/dannygroupllc/ConsultantWebService/ConsultantWebServiceApplication.java
@@ -4,15 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.sql.Timestamp;
-
 @SpringBootApplication
 @EnableScheduling
 public class ConsultantWebServiceApplication {
 
 	// public static String machineIp = "3.20.119.226";
 	// public static String machineIp = "192.168.43.132";
-	// test
 	public static void main(String[] args) {
 		SpringApplication.run(ConsultantWebServiceApplication.class, args);
 	}
